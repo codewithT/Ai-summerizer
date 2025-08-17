73d68411807da7062df547d0d141db66b26f2d8a
codewithT <tulasirammadaka403@gmail.com>
2025-08-17 11:11:04 +0530

    updated with cors

--- meeting-notes-app/backend/server.js
+++ meeting-notes-app/backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://free-ai-summarizer.netlify.app'
+  'https://free-ai-summerizer.netlify.app/'
 ];
 
 app.use(cors({
